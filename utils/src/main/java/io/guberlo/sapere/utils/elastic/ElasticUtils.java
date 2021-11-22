28957325dcee2b76e5a29e635752399ebb15ede0
Guberlo <salvo.asero@neodatagroup.com>
2021-11-22 11:07:13 +0100

    fix: elastic search communication

--- utils/src/main/java/io/guberlo/sapere/utils/elastic/ElasticUtils.java
+++ utils/src/main/java/io/guberlo/sapere/utils/elastic/ElasticUtils.java
@@ -1,12 +1,10 @@
 package io.guberlo.sapere.utils.elastic;
 
-import io.guberlo.sapere.utils.config.Config;
 import org.apache.http.HttpHost;
 import org.apache.spark.sql.Row;
 import org.elasticsearch.ElasticsearchException;
 import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.action.update.UpdateResponse;
-import org.elasticsearch.client.Request;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
@@ -23,31 +21,29 @@ public class ElasticUtils implements Serializable {
 
     private final static Logger LOG = LoggerFactory.getLogger(ElasticUtils.class);
     private transient static RestHighLevelClient client;
-    private final transient Config config;
+    private final String index;
 
-    public ElasticUtils(Config config) {
-        this.config = config;
-        LOG.warn(config.toString());
+    public ElasticUtils(String index, String host) {
+        this.index = index;
         try {
             client = new RestHighLevelClient(RestClient.builder(
-                    new HttpHost(config.getElasticConfig().getHost(), 9200, "http")
+                    new HttpHost(host, 9200, "http")
             ));
         } catch (Exception e) {
-            LOG.warn(config.toString());
+            LOG.error("Connecting to Elastic Search: {}", e.getMessage());
         }
     }
 
     public void updateOnES(Iterator<Row> data) {
         data.forEachRemaining(row -> {
-            Integer id = row.getAs("id");
+            String id = row.getAs("id");
             String type = row.getAs("type");
             String prediction = row.getAs("prediction");
-            UpdateRequest request = new UpdateRequest(config.getElasticConfig().getIndex(), String.valueOf(id));
+            UpdateRequest request = new UpdateRequest(index, id);
             String body = "{" +
-                    "\"doc\":{" +
                     "\"" + type + "\":" +
-                    "\"" + prediction + "\"" +
-                    "}}";
+                    "\"" + prediction.replaceAll("\"", "'") + "\"" +
+                    "}";
 
             request.doc(body, XContentType.JSON);
 
@@ -60,6 +56,7 @@ public class ElasticUtils implements Serializable {
                 }
             } catch (IOException e) {
                 LOG.error("Error on updating elastic search doc: {}", e.getMessage());
+                LOG.error("Request body is: {}", body);
             }
         });
     }
